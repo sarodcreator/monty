08b5aa6d11b5cba787f3d110c96c68b05a2b7a00
barioo saro <sarobaridoo@gmail.com>
2023-12-14 13:49:53 -0800

    monty

---
+++ builtin1.c
@@ -0,0 +1,159 @@
+#include "monty.h"
+/*
+  * _pall - prints the stacks
+  * @front: stack head
+  * @count: no used
+  * Return: no return
+  */
+
+void _pall (stack_t  **front, unsigned int count)
+{
+	stack_t  *f;
+	
+	(void)count;
+	f  = *front;
+	
+	if  (f != NULL)
+	{
+		while (f)
+		{
+			printf("%d\n", (*f).n);
+			f = (*f).next;
+		}
+	}
+	else
+	{
+		return;
+	}
+}
+
+/**
+ * _push - adds node to the stack
+ * @front: double head pointer to the stack
+ * @count: line count
+ *
+ * Return: nothing
+ */
+
+void _push(stack_t **front, unsigned int count)
+{
+	int d, n, flag;
+
+	n = 0;
+	flag = 0;
+
+	if (!global.token)
+	{
+		fprintf(stderr, "L%d: usage : push integer\n", count);
+		fclose (global.fd);
+		free (global.content);
+		free_stack(EXIT_FAILURE);
+	}
+	else
+	{
+		if (global.token[0] == '-')
+			n++;
+		for (; global.token[n] > 57 || global.token[n] <  48)
+			flag = 1;
+		if (flag > 0 && flag < 2)
+		{
+				fprintf (stderr, "L%d: usage: Push integer\n", count);
+				free (global.fd);
+				free_stack(*front);
+				exit(EXIT_FAILURE);
+		}
+	}
+	d = atoi(global.token);
+	
+	if (global.token != 0)
+	{
+		addqueue(*front, d);
+	}
+	else
+	{
+		addnode(front, d);
+	}
+}
+
+/**
+ * _pint - prints last node on stack
+ * @front: pointer to pointer of first node
+ * @count: line counter
+ *
+ * Return: none
+ */
+void _pint (stack_t *front, unsigned int count)
+{
+	if (*front == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", count);
+		fclose(global.fd);
+		free(global.content);
+		free-stack(*front);
+		exit (EXIT_FAILURE);
+	}
+	printf("%d\n", (*(*front)).n)
+}
+
+/**
+ * _pop - removes top node on stack
+ * @front: pointer to pointer of first node
+ * @count: line counter
+ *
+ * Return: none
+ */
+
+void _pop(stack_t **front, unsigned int count)
+{
+	stack_t *f;
+	if (*front == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", count);
+		fclose(global.fd);
+		free(global.content);
+		free_stack(*front);
+		exit(EXIT_FAILURES);
+	}
+
+	f = *front;
+	*head = (*f).next;
+	free(f);
+}
+
+/**
+* _swap - swaps first two elements of stack
+* @front: first node
+* @count: line counter
+*
+* Return: none
+*/
+
+void _swap (stack_t **front, unsigned int count)
+{
+	stack_t *f;
+	stack_t *new_node;
+	
+	int len = 0;
+	temp;
+
+	f = *front;
+
+	while(f)
+	{
+		f = (*f).next;
+		len++;
+	}
+	if (len <=  1)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", count);
+		fclose(global.fd);
+		free(global.content);
+		free_stack(*front);
+		exit(EXIT_FAILURE);
+	}
+	f = *front;
+	temp = (*f).n;
+	new_node = f->next;
+	f->n = new_node->n;
+	new_node->n = temp;
+}
---
+++ monty.h
@@ -0,0 +1,108 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+       	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * global_var - struct with variable globals
+ * @token: integer
+ * fd: pointer to monty file
+ * @content: line content
+ * @lifo: flag change stack <-> queue
+ *
+ *Description: carries values through entire program
+ */
+
+typedef struct global_var
+{
+	char *token;
+	FILE *fd;
+	char *content;
+	int lifo;
+}   global_var_t;
+extern global_var_t  global;
+
+/**______builtin1.c______*/
+void _pall (stack_t  **front, unsigned int count);
+void _push(stack_t **front, unsigned int count);
+void _pint (stack_t *front, unsigned int count);
+void _pop(stack_t **front, unsigned int count);
+void _swap (stack_t **front, unsigned int count);
+
+/**______builtin2.c______*/
+void _nop(stack-t **front, unsigned int count);
+
+/**______add-func.c_____*/
+void _add (stack_t **front, unsigned int count);
+
+/**______sub_func.c______*/
+void _sub(stack_t **front, unsigned int count);
+
+/**______div-func.c______*/
+void _div(stack_t **front, unsigned int count);
+
+/**______mul-func.c______*/
+void _mul(stack_t **front, unsigned int count);
+
+/**______mod-func.c______*/
+void _mod(stack_t **front, unsigned int count);
+
+/**______pchar.c______*/
+void _pchar(stack_t **front, unsigned int count);
+
+/**______pstr.c______*/
+void _pstr(stack_t **front, unsigned int count);
+
+/**______rotr.c______*/
+void _rotr(stack_t **front, __attribute__((unused)) unsigned int count);
+
+/**______rotl.c______*/
+void _rotl(stack_t **front, __attribute__((unused)) unsigned int count);
+
+/**______struct.c______*/
+void _stack(stack_t **front, unsigned int count);
+void _queue(stack_t **head, unsigned int count);
+
+/**____free_stack.c___*/
+void free_stack(stack_t *front);
+
+/**______add_queue.c______*/
+void addqueue(stack_t **front, int n);
+
+/**______add_node.c______*/
+void addnode(stack_t **front, int n);
+
+/**______exe.c______*/
+int exe(char *content, stack_t **stack, unsigned int count, FILE *fd);
+
+#endif
